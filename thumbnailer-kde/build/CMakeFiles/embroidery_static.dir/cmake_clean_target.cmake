bacbeb1b3d9ef70d3cb07ef7cd54b28e84588ce5
Nick Savchenko <naiksav4enko@gmail.com>
2025-08-15 03:26:54 +0300

    Remove build directory from version control
    Build artifacts should not be tracked in Git and are now properly ignored

--- thumbnailer-kde/build/CMakeFiles/embroidery_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libembroidery_static.a"
-)
--- thumbnailer-kde/build/CMakeFiles/embroidery_static.dir/embroidery_static_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/embroidery_static.dir/embroidery_static_autogen/mocs_compilation.cpp.o: \
- /home/naik/Dev/Embroidermodder/thumbnailer-kde/build/embroidery_static_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
